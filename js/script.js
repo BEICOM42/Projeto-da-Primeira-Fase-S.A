4bfea672f10034e538ce5e5f5993b902f3ee3420
Diego Henrique De Castro Hessel <diego_hessel@estudante.sesisenai.org.br>
2025-08-21 16:48:44 -0300

    subindo arquivo Diego(1) Data:21/08/2025

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function escolherOpcao(opcao) {
+  if (opcao === "paciente") {
+    alert("Você escolheu criar conta como Paciente!");
+  } else if (opcao === "medico") {
+    alert("Você escolheu criar conta como Médico!");
+  } else if (opcao === "login") {
+    alert("Você escolheu entrar em uma conta existente!");
+  }
+}
+function escolherOpcaoCima(opcao){
+  if (opcao === 'inicio')
+    alert("você escolheu o batão de inicio")
+  else if (opcao === 'conversa'){
+    alert('conversa')
+  }
+  else if (opcao === 'atividade'){
+    alert('atividade')
+  }
+  else if (opcao === 'info'){
+    alert('info')
+  }
+}
